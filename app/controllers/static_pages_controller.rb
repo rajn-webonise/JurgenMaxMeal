545b51cb46bb8e2dd5f6dab1074bbd4331da2281
rajn-webonise <raj.negi@weboniselab.com>
2016-08-06 19:18:13 +0530

    Made final UI changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,61 +1,6 @@
 
 class StaticPagesController < ApplicationController
 
-  def checkifmin(data, input, selected)
-    #debugger if !selected.include? 6
-    inputs = input.clone
-    inputsize = inputs.size
-    cost = 0
-    contains = 0
-
-    selected.each do |current_menu_id|
-
-      cost += data[current_menu_id][1].to_f
-
-      for current_item_id in 2..(data[current_menu_id].size-1)
-
-          if inputs.include? data[current_menu_id][current_item_id]
-            contains+=1
-            inputs.delete data[current_menu_id][current_item_id]
-          end
-
-      end
-
-      if contains >= inputsize && contains>0 && params["answer"] > cost
-        params["answer"] = cost
-        #debugger
-      end
-
-    end
-
-  end
-
-  # This algorithm generates all possible inputs
-  def algorithm(data, input, selected, depth)
-
-    if depth >= data.size
-      return
-    end
-
-    checkifmin(data, input, selected)
-
-    selected.push depth
-    algorithm(data, input, selected, depth+1)
-
-    checkifmin(data, input, selected)
-
-    selected.pop
-
-    algorithm(data, input, selected, depth+1)
-    #debugger if input.size==0
-    checkifmin(data, input, selected)
-
-  end
-
-  def solve(datas, input)
-    selected = Array.new
-    algorithm(datas, input, selected, 0)
-  end
 
   def home
   end
@@ -66,25 +11,20 @@ class StaticPagesController < ApplicationController
   def combo_cost_evaluator(current_combo)
     inputs = params["q"].split(" ")
     cost = 0.0
-#    debugger if current_combo.size == 2
-    #loop through each menu in a combo
     current_combo.each do |menu|
       cost += menu.price.to_f
       items = Item.where(mid: menu.id).to_a
       #loop through each item in a menu
       items.each do |item|
-  #      debugger if current_combo.size == 2
         if inputs.include? item.name
           inputs.delete item.name
         end
 
       end
-#     debugger if current_combo.size == 2
     end
     if inputs.size!=0
       cost = -1.0
     end
-#    debugger if current_combo.size == 2
     return cost
   end
 
@@ -114,10 +54,13 @@ class StaticPagesController < ApplicationController
         end
       end
 
-      #debugger
     end
-    render text: "br><br>Minimum cost: "+ minimum_cost.to_s + " and mids: " + minimum_mids.to_s
 
+    if minimum_cost>0
+      params["output"] = "You can eat at restaurant#ID " + Restaurant.find_by(id: Menu.find_by( id: (minimum_mids[0].id).to_i ).rid ).title + " for a price of " + minimum_cost.to_s
+    else
+      params["output"] = "Sorry, no restaurant serves this combination!"
+    end
     #  answer = solve(data, params["q"].split(","))#  render text: data.to_s + "<br><br>Minimum cost: " + params["answer"].to_s
   end
 
